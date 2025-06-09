36f6e2f5939ba0fc2b922357a17d38a50d0b93fd
harangji <radio000507@gmail.com>
2025-06-09 17:59:39 +0900

    턴 로직 초안 작성중

--- Assets/3.Script/Ji/TurnManager.cs
+++ Assets/3.Script/Ji/TurnManager.cs
@@ -1,136 +1,165 @@
-// using System;
-// using System.Threading.Tasks;
-// using UnityEngine;
-//
-// namespace _3.Script.Ji
-// {
-//     public class TurnManager : MonoBehaviour
-//     {
-//         public int MAX_TURN_COUNT { private get; set; } = 5;
-//     
-//         public static TurnManager Instance { get; private set; }
-//     
-//         public event EventHandler<ActorParent> ActorChanged;
-//         public event EventHandler<GameStateEventArgs> GameStateChanged;
-//     
-//         public GameState State { get; private set; } = GameState.Waiting;
-//         public ActorParent CurrentTurn { get; private set; } = ActorParent.None;
-//         public int TurnCount { get; private set; } = 0;
-//
-//         public TaskCompletionSource<bool> TurnEndedSource;
-//
-//         private void Awake()
-//         {
-//             if (Instance != null && Instance != this)
-//             {
-//                 Destroy(gameObject);
-//                 return;
-//             }
-//             Instance = this;
-//             DontDestroyOnLoad(gameObject);
-//         }
-//
-//         private void Start()
-//         {
-//             //if 이니셜라이즈 오브젝트가 완료면
-//             CurrentTurn = ActorParent.Player;
-//             _ = RunGameFlow();
-//         }
-//
-//         #region Server Logic
-//
-//         private async Task RunGameFlow()
-//         {
-//             await Task.Delay(1000);
-//         
-//             while (State == GameState.Playing)
-//             {
-//                 SetNextTurn();
-//                 TurnEndedSource = new TaskCompletionSource<bool>();
-//                 await TurnEndedSource.Task;
-//             
-//                 SetNextTurn();
-//                 TurnEndedSource = new TaskCompletionSource<bool>();
-//                 await TurnEndedSource.Task;
-//
-//                 TurnCount++;
-//
-//                 if (CheckWinCondition(out var winner))
-//                 {
-//                     EndGame(winner);
-//                     break;
-//                 }
-//             }
-//         }
-//     
-//         private void SetNextTurn() //턴 전환
-//         {
-//             CurrentTurn = (CurrentTurn == ActorParent.Player) ? ActorParent.Enemy : ActorParent.Player;
-//             Debug.Log($"턴 전환됨: {CurrentTurn}");
-//         }
-//     
-//         private bool CheckWinCondition(out ActorParent winner)
-//         {
-//             if (TurnCount >= MAX_TURN_COUNT)
-//             {
-//                 winner = DetermineWinnerByScore();
-//                 return true;
-//             }
-//     
-//             winner = ActorParent.None;
-//             return false;
-//         }
-//
-//         private ActorParent DetermineWinnerByScore()
-//         {
-//             return ActorParent.None;
-//         }
-//
-//         private void EndGame(ActorParent winner)
-//         {
-//             Debug.Log($"게임 종료. 승자: {winner}");
-//             State = GameState.Ended;
-//             GameStateChanged?.Invoke(this, new GameStateEventArgs(State));
-//         }
-//     
-//         private void RpcTurnStart_S2C(ActorParent actor)
-//         {
-//             CurrentTurn = actor;
-//             ActorChanged?.Invoke(this, CurrentTurn);
-//         }
-//         #endregion
-//     
-//         // #region Server To Client | (Client Logic)
-//         //
-//         // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
-//         // public void RpcSetScoreBoard_S2C()
-//         // {
-//         //     ScoreManager.Instance.HostBoard = new ScoreManager.ScoreBoard(YachtPlayer.HostPlayer.PlayerRef);
-//         //     ScoreManager.Instance.ClientBoard = new ScoreManager.ScoreBoard(YachtPlayer.ClientPlayer.PlayerRef);
-//         // }
-//         //
-//         // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
-//         // public void RpcStartGame_S2C()
-//         // {
-//         //     Debug.Log("게임 시작");
-//         //     State = GameState.Playing;
-//         //     GameStateChanged?.Invoke(this, new GameStateEventArgs(State));
-//         // }
-//         //
-//         // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
-//
-//         //
-//         // #endregion
-//     
-//         // #region Client To Server | (Server Logic)
-//         //
-//         // [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
-//         // public void RpcTurnEnd_C2S(PlayerRef playerRef)
-//         // {
-//         //     turnEndedSource?.TrySetResult(true);
-//         // }
-//         //
-//         // #endregion
-//     }
-// }
-//
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _3.Script.Ji
+{
+    public partial class TurnManager : MonoBehaviour
+    {
+        public static TurnManager Instance { get; private set; }
+    
+        public event EventHandler<ActorParent> ActorChanged;
+        public event EventHandler<GameStateEventArgs> GameStateChanged;
+    
+        public GameState State { get; private set; } = GameState.Waiting;
+        public ActorParent CurrentTurn { get; private set; } = ActorParent.None;
+        public int TurnCount { get; private set; } = 0;
+
+        public TaskCompletionSource<bool> TurnEndedSource;
+        
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            //비동기로 게임 초기화를 기다립니다.
+            InGameInitialize().ContinueWithOnMainThread(task =>
+            {
+                if (task.IsFaulted || task.IsCanceled) return;
+                //--초기화 완료 시점--
+                _= RunGameFlow();
+            });
+        }
+        
+        private async Task RunGameFlow()
+        {
+            await Task.Delay(1000);
+        
+            StartGame();
+            
+            while (State == GameState.Playing)
+            {
+                SetNextTurn();
+                TurnEndedSource = new TaskCompletionSource<bool>();
+                await TurnEndedSource.Task;
+            
+                if (CheckWinCondition())
+                {
+                    break;
+                }
+                
+                SetNextTurn();
+                TurnEndedSource = new TaskCompletionSource<bool>();
+                await TurnEndedSource.Task;
+
+                TurnCount++;
+
+                if (CheckWinCondition())
+                {
+                    break;
+                }
+            }
+        }
+    
+        private void SetNextTurn() //턴 전환
+        {
+            //처음 실행되면 ActorParent.None 이므로 플레이어부터 시작
+            CurrentTurn = (CurrentTurn == ActorParent.Player) ? ActorParent.Enemy : ActorParent.Player;
+            Debug.Log($"턴 전환됨: {CurrentTurn}");
+            TurnStart(CurrentTurn);
+        }
+    
+        private bool CheckWinCondition() //승자가 나올 겨우 true, 아니라면 false
+        {
+            return false; //please fix
+            
+            ActorParent winner;
+            
+            // 추가할 것 - 양쪽에 //&& 맵 승리조건이 있고, 그게 달성되었으면 && mapWinLogic?.Invoke ?
+            
+            if (playerUnits.All(unit => unit.isDead)) //playerAllDead
+            {
+                winner = ActorParent.Enemy;
+                EndGame(winner);
+                return true;
+            }
+            
+            if (monsterUnits.All(unit => unit.isDead)) //enemyAllDead
+            {
+                winner = ActorParent.Player;
+                EndGame(winner);
+                return true;
+            }
+            
+            if (TurnCount >= maxTurnCount) // 턴이 최대 턴 수를 지나 패배 처리
+            {
+                winner = ActorParent.Enemy;
+                EndGame(winner);
+                return true;
+            }
+            
+            winner = ActorParent.None;
+            return false;
+        }
+
+
+        private void StartGame()
+        {
+            Debug.Log("게임 시작");
+            State = GameState.Playing;
+            GameStateChanged?.Invoke(this, new GameStateEventArgs(State));
+        }
+        
+        private void TurnStart(ActorParent actor)
+        {
+            CurrentTurn = actor;
+            ActorChanged?.Invoke(this, CurrentTurn);
+        }
+        
+        private void EndGame(ActorParent winner)
+        {
+            Debug.Log($"게임 종료. 승자: {winner}");
+            State = GameState.Ended;
+            GameStateChanged?.Invoke(this, new GameStateEventArgs(State));
+        }
+        
+        // #region Server To Client | (Client Logic)
+        //
+        // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
+        // public void RpcSetScoreBoard_S2C()
+        // {
+        //     ScoreManager.Instance.HostBoard = new ScoreManager.ScoreBoard(YachtPlayer.HostPlayer.PlayerRef);
+        //     ScoreManager.Instance.ClientBoard = new ScoreManager.ScoreBoard(YachtPlayer.ClientPlayer.PlayerRef);
+        // }
+        //
+        // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
+
+        //
+        // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
+
+        //
+        // #endregion
+    
+        // #region Client To Server | (Server Logic)
+        //
+        // [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
+        // public void RpcTurnEnd_C2S(PlayerRef playerRef)
+        // {
+        //     turnEndedSource?.TrySetResult(true);
+        // }
+        //
+        // #endregion
+    }
+}
+
