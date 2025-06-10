4bd9b02850e06c2124063a29c3698bda2606a977
yunnozee <nohyunji127@gmail.com>
2025-06-10 17:42:09 +0900

    플레이어 프리팹 드래그 기능 추가

--- Assets/3.Script/No/Character3DDragSystem.cs
+++ Assets/3.Script/No/Character3DDragSystem.cs
@@ -3,47 +3,58 @@ using UnityEngine.EventSystems;
 
 public class Character3DDragSystem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private Canvas canvas;
-    private RectTransform rectTransform;
-    private CanvasGroup canvasGroup;
-    private Vector3 originalPosition;
-
-    public GameObject characterPrefab3D;
+    private Camera mainCamera;
+    private Vector3 offset;
+    private bool isDragging;
 
     private void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
-        canvasGroup = GetComponent<CanvasGroup>();
-        canvas = GetComponentInParent<Canvas>();
+        mainCamera = Camera.main;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        originalPosition = rectTransform.anchoredPosition;
-        canvasGroup.blocksRaycasts = false;
-        canvasGroup.alpha = 0.6f;
+        Debug.Log("OnBeginDrag");
+        
+        isDragging = true;
+
+        Ray ray = mainCamera.ScreenPointToRay(eventData.position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            offset = transform.position - hit.point;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        if (!isDragging) return;
+
+        Ray ray = mainCamera.ScreenPointToRay(eventData.position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 targetPos = hit.point + offset;
+            transform.position = new Vector3(targetPos.x, 1f, targetPos.z);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.blocksRaycasts = true;
-        canvasGroup.alpha = 1f;
-        rectTransform.anchoredPosition = originalPosition;
+        isDragging = false;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = mainCamera.ScreenPointToRay(eventData.position);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Tile tile = hit.collider.GetComponent<Tile>();
-
-            if (tile.tileType != 1) return;
-            
-            Instantiate(characterPrefab3D, hit.collider.transform.position + Vector3.up, Quaternion.identity);
-            Debug.Log("캐릭터 배치 완료");
+            Tile targetTile = TileManager.Instance.GetTileAtWorldPosition(hit.point);
+            if (targetTile.tileType == 1) 
+            {
+                transform.position = new Vector3(targetTile.transform.position.x, 1f, targetTile.transform.position.z);
+                Debug.Log("Valid tile. Character moved.");
+            }
+            else
+            {
+                Debug.Log("Invalid tile. Character not moved.");
+                // 원래 위치로 되돌릴 수도 있음
+            }
         }
     }
 }
