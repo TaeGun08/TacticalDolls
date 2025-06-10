167f6352b41fda7f03e2d53d747e962c97126233
yunnozee <nohyunji127@gmail.com>
2025-06-10 10:03:11 +0900

    더미 플레이어 생성 적용

--- Assets/3.Script/No/PlayerManager.cs
+++ Assets/3.Script/No/PlayerManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerManager: MonoBehaviour
@@ -10,40 +12,123 @@ public class PlayerManager: MonoBehaviour
     public class PlayerData
     {
         // player 정보
-        public string Email;
-        public string NickName;
-        public string Role;
-        public bool IsTutorialCompleted;
-        public int RankPoint;
-        public List<string> Friends = new();
-        public List<string> MatchHistorys = new();
-        public List<int> ChracterId;
+        // public string Email;
+        // public string NickName;
+        // public string Role;
+        // public bool IsTutorialCompleted;
+        // public int RankPoint;
+        // public List<string> Friends = new();
+        // public List<string> MatchHistorys = new();
+        // public List<int> ChracterId;
         
         // character 정보
-        public List<CharacterData> Characters = new();
+        //public List<PlayerDataSample> player = new();
     }
 
+    public PlayerDataSample player;
+    
     private void Start()
     {
-        PlayerData  playerData = new PlayerData();
+        player = new PlayerDataSample();
         
-        playerData.Characters.Add(CharacterPrefab[0]);
-
-        Instantiate(CharacterPrefab[0].gameObject, new Vector3(0, 1, 0), Quaternion.identity);
+        TestPlayerHasCharacter();
         
-        Debug.Log($"playerData.Characters[0].CharacterID::{playerData.Characters[0].CharacterID}");
+        //player[0].HasCharacter.
+        // PlayerData  player = new PlayerData();
+        //
+        // playerData.Characters.Add(CharacterPrefab[0]);
+        //
+        // Instantiate(CharacterPrefab[0].gameObject, new Vector3(0, 1, 0), Quaternion.identity);
+        //
+        // Debug.Log($"playerData.Characters[0].CharacterID::{playerData.Characters[0].CharacterID}");
+        //
+        // for (int i = 0; i < playerData.Characters.Count; i++)
+        // {
+        //     foreach (SkillSO skill in playerData.Characters[i].Skills)
+        //     {
+        //         Debug.Log("Skill info");
+        //         Debug.Log($"Name::{skill.Name}");
+        //         Debug.Log($"Name::{skill.SkillID}");
+        //         Debug.Log($"Name::{skill.Type}");
+        //         Debug.Log($"Name::{skill.Range}");
+        //     }
+        // }
 
-        for (int i = 0; i < playerData.Characters.Count; i++)
+    }
+
+    public void TestPlayerHasCharacter()
+    {
+        CharacterDataSample sampleCharacter = new CharacterDataSample
         {
-            foreach (SkillSO skill in playerData.Characters[i].Skills)
+            characterCode = 0,
+            level = 1,
+                
+            weapon = new WeaponDataSample
             {
-                Debug.Log("Skill info");
-                Debug.Log($"Name::{skill.Name}");
-                Debug.Log($"Name::{skill.SkillID}");
-                Debug.Log($"Name::{skill.Type}");
-                Debug.Log($"Name::{skill.Range}");
+                weaponCode = 0,
+                level = 1,
+            },
+                
+            skills = new SkillDataSample[2]
+            {
+                new SkillDataSample
+                {
+                    skillCode = 1,
+                    level = 1
+                },
+                new SkillDataSample
+                {
+                    skillCode = 2,
+                    level = 3
+                }
+            }
+        };
+        
+        CharacterDataSample sampleCharacter2 = new CharacterDataSample
+        {
+            characterCode = 2,
+            level = 1,
+                
+            weapon = new WeaponDataSample
+            {
+                weaponCode = 0,
+                level = 1,
+            },
+                
+            skills = new SkillDataSample[2]
+            {
+                new SkillDataSample
+                {
+                    skillCode = 0,
+                    level = 1
+                },
+                new SkillDataSample
+                {
+                    skillCode = 2,
+                    level = 3
+                }
+            }
+        };
+        
+        player.HasCharacter.Add(sampleCharacter); 
+        player.HasCharacter.Add(sampleCharacter2); 
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log($"player.HasCharacter[0].characterCode:: {player.HasCharacter[0].characterCode}");
+
+            int pos = 0; 
+            
+            foreach (var characterDataSample in player.HasCharacter)
+            {
+                Instantiate(CharacterPrefab[characterDataSample.characterCode].gameObject, new Vector3(pos, 1, 0),
+                    Quaternion.identity);
+
+                pos++;
             }
         }
-
     }
 }
