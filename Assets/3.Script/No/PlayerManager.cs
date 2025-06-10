c470e65ffbbab2d033b27811269e95701d4a6f14
bychan0v0 <bychan0v0@gmail.com>
2025-06-10 10:47:00 +0900

    캐릭터 소환 스페이스에서 숫자 1로 변경

--- Assets/3.Script/No/PlayerManager.cs
+++ Assets/3.Script/No/PlayerManager.cs
@@ -116,7 +116,7 @@ public class PlayerManager: MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log($"player.HasCharacter[0].characterCode:: {player.HasCharacter[0].characterCode}");
 
