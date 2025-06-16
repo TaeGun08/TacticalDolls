f68e7a9abc55193ca5efbee3e3e7632045169fb6
harangji <radio000507@gmail.com>
2025-06-16 20:05:29 +0900

    추가

--- Assets/3.Script/Ji/TimeLine/DynamicTransformTween.cs
+++ Assets/3.Script/Ji/TimeLine/DynamicTransformTween.cs
@@ -1,21 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Firebase.Extensions;
 using UnityEngine;
 using UnityEngine.Playables;
-using UnityEngine.Serialization;
 using UnityEngine.Timeline;
-using Object = UnityEngine.Object;
 
-public enum TrackName
-{
-    Move,
-    Fire,
-    FireBezier,
-}
 
 public class DynamicTransformTweenChanger : MonoBehaviour
 {
@@ -29,17 +16,18 @@ public class DynamicTransformTweenChanger : MonoBehaviour
     public Transform testplayer;
     public Transform shotTransform;
     
-    private CustomBezierCurveTweenClip cashTweenClip;
+    private PlayableAsset cashTweenClip;
     private string targetTrack;
 
     public PlayableDirector testDirector;
     public TimelineAsset Target;
     public TimelineAsset NewSourceTimelineAsset;
-    // public TimelineClipModifier modifier;
+    public Camera camera;
     
     private void Awake()
     {
         Instance = this;
+        camera = Camera.main;
     }
 
     private void Update()
@@ -50,58 +38,62 @@ public class DynamicTransformTweenChanger : MonoBehaviour
         //     // CopyClipsFromTo(NewSourceTimelineAsset, Target);
         // }
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            // ChangeLocation(testDirector, testTransform);
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                // hit.point를 Transform으로 바꾸기 위해 임시 오브젝트 생성
+                GameObject tempTarget = new GameObject("TempHitTarget");
+                tempTarget.transform.position = hit.point;
+
+                // Transform으로 전달
+                ChangeTransformTweenLocation(testDirector, null, tempTarget.transform);
+            }
+            
             CopyClipsFromTo(NewSourceTimelineAsset, Target);
             ChangeBezierTweenLocation(testDirector, testplayer, shotTransform);
         }
     }
 
     //클립의 로케이션 변경
-    // public void ChangeTransformTweenLocation(PlayableDirector director, Transform newStartLocation, Transform newEndLocation)
-    // {
-    //     cashTweenClip = FindClipByName<TransformTweenClip>(director, TrackName.Move);
-    //     
-    //     if (cashTweenClip is TransformTweenClip tweenClip) //형변환
-    //     {
-    //         // 새 Location을 PlayableDirector에 등록
-    //         Debug.Log(newStartLocation.gameObject.name);
-    //         Debug.Log(newEndLocation.gameObject.name);
-    //         director.SetReferenceValue(tweenClip.startLocation.exposedName, newStartLocation); //동적할당 하기 위해선 exposedName사용
-    //         director.SetReferenceValue(tweenClip.endLocation.exposedName, newEndLocation);
-    //     }
-    //     
-    //     // 변경사항 적용
-    //     director.RebuildGraph();
-    //     director.Play();
-    // }
-    
-    public void ChangeBezierTweenLocation(PlayableDirector director, Transform newStartLocation, Transform newEndLocation)
+    public void ChangeTransformTweenLocation(PlayableDirector director, Transform newStartLocation = null, Transform newEndLocation = null)
     {
-        cashTweenClip = FindClipByName(director, TrackName.FireBezier);
+        cashTweenClip = FindClipByType<TransformTweenClip>(director);
         
-        Debug.Log($"tweenClip.startLocation.exposedName : {cashTweenClip.startLocation.exposedName}");
-        Debug.Log($"tweenClip.endLocation.exposedName : {cashTweenClip.endLocation.exposedName}");
-        // 새 Location을 PlayableDirector에 등록
-        director.SetReferenceValue(cashTweenClip.startLocation.exposedName, newStartLocation); //동적할당 하기 위해선 exposedName사용
-        director.SetReferenceValue(cashTweenClip.endLocation.exposedName, newEndLocation);
-        
-        // if (cashTweenClip is CustomBezierCurveTweenClip tweenClip) //형변환
-        // {
-        //     Debug.Log($"tweenClip.startLocation.exposedName : {tweenClip.startLocation.exposedName}");
-        //     Debug.Log($"tweenClip.endLocation.exposedName : {tweenClip.endLocation.exposedName}");
-        //     // 새 Location을 PlayableDirector에 등록
-        //     director.SetReferenceValue(tweenClip.startLocation.exposedName, newStartLocation); //동적할당 하기 위해선 exposedName사용
-        //     director.SetReferenceValue(tweenClip.endLocation.exposedName, newEndLocation);
-        // }
+        if (cashTweenClip is TransformTweenClip tweenClip) //형변환
+        {
+            // 새 Location을 PlayableDirector에 등록
+            //start필요없긴하지만 테스트용으로
+            if(newStartLocation != null)
+                director.SetReferenceValue(tweenClip.startLocation.exposedName, newStartLocation); //동적할당 하기 위해선 exposedName사용
+            director.SetReferenceValue(tweenClip.endLocation.exposedName, newEndLocation);
+        }
         
         // 변경사항 적용
         director.RebuildGraph();
         director.Play();
     }
     
-    private CustomBezierCurveTweenClip FindClipByName(PlayableDirector director, TrackName trackName) //탄환 도착 지점 동적할당
+    public void ChangeBezierTweenLocation(PlayableDirector director, Transform newStartLocation = null, Transform newEndLocation = null)
+    {
+        cashTweenClip = FindClipByType<CustomBezierCurveTweenTrack>(director) as CustomBezierCurveTweenClip;
+        
+        if (cashTweenClip is CustomBezierCurveTweenClip tweenClip) //형변환
+        {
+            // 새 Location을 PlayableDirector에 등록
+            if(newStartLocation != null)
+                director.SetReferenceValue(tweenClip.startLocation.exposedName, newStartLocation); //동적할당 하기 위해선 exposedName사용
+            director.SetReferenceValue(tweenClip.endLocation.exposedName, newEndLocation);
+        }
+        
+        // 변경사항 적용
+        director.RebuildGraph();
+        director.Play();
+    }
+    
+    private PlayableAsset FindClipByType<T>(PlayableDirector director) //탄환 도착 지점 동적할당
     {
         TimelineAsset timeline = director.playableAsset as TimelineAsset;
 
@@ -110,41 +102,15 @@ public class DynamicTransformTweenChanger : MonoBehaviour
             Debug.LogError(" missing timeline asset");
             return null;
         }
-
-        Type trackType;
-        switch (trackName)
-        {
-            case TrackName.Move:
-                targetTrack = MOVE_TWEEN_TRACK;
-                trackType  = typeof(TransformTweenTrack);
-                break;
-            case TrackName.Fire:
-                targetTrack = FIRE_PROJECTILE_TRACK;
-                trackType  = typeof(CustomBezierCurveTweenTrack);
-                break;
-            case TrackName.FireBezier:
-                targetTrack = CUSTOM_BEZIER_CURVE_TWEEN_TRACK;
-                trackType  = typeof(CustomBezierCurveTweenTrack);
-                break;
-            default:
-                targetTrack = "missing";
-                trackType  = typeof(CustomBezierCurveTweenTrack);
-                break;
-        }
-
+        
         foreach (TrackAsset track in timeline.GetOutputTracks()) //트랙 찾기
         {
-            if(track.name != targetTrack) continue; //이름으로 트랙 찾기
-            // if(track.GetType() != typeof(T)) continue;
+            // if(track.name != targetTrack) continue; //이름으로 트랙 찾기
+            if(track.GetType() != typeof(T)) continue;
             
             foreach (TimelineClip clip in track.GetClips()) //클립 찾기
             {
-                //TransformTweenClip 타입인 클립만 처리
-                if (clip.asset is CustomBezierCurveTweenClip tweenClip)
-                {
-                    Debug.Log(tweenClip.name);
-                    return tweenClip;
-                }
+                return clip.asset as PlayableAsset; //일반화
             }
         }
         
@@ -154,6 +120,7 @@ public class DynamicTransformTweenChanger : MonoBehaviour
     
     public void CopyClipsFromTo(TimelineAsset newSource, TimelineAsset target)
     {
+        //최적화 필요
         foreach (TrackAsset oldTrack in target.GetOutputTracks())
         {
             foreach (var oldClip in oldTrack.GetClips())
@@ -174,6 +141,36 @@ public class DynamicTransformTweenChanger : MonoBehaviour
                 }
             }
         }
+        
+        // foreach (var track in target.GetOutputTracks())
+        // {
+        //     if (track is GroupTrack groupTrack)
+        //     {
+        //         Debug.Log("Group: " + groupTrack.name);
+        //         foreach (var child in groupTrack.GetChildTracks())
+        //         {
+        //             Debug.Log($"  └── Child Track: {child.name}");
+        //         }
+        //     }
+        // }
+        //
+        // foreach (TrackAsset oldTrack in target.GetOutputTracks())
+        // {
+        //     foreach (var newSourceTrack in newSource.GetOutputTracks())
+        //     {
+        //         if(oldTrack.GetType() != newSourceTrack.GetType()) continue;
+        //
+        //         foreach (TimelineClip oldClip in oldTrack.GetClips())
+        //         {
+        //             TimelineClip newClip = newSourceTrack.GetClips().First();
+        //             
+        //             oldClip.start = newClip.start;
+        //             oldClip.duration = newClip.duration;
+        //             oldClip.displayName = newClip.displayName;
+        //             oldClip.asset = newClip.asset;
+        //         }
+        //     }
+        // }
         
         // return Task.CompletedTask;
         // testDirector.RebuildGraph();
