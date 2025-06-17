af533b3738b303600911b3516018496fb09468f4
bychan0v0 <bychan0v0@gmail.com>
2025-06-17 15:52:56 +0900

    기본 페이지 이동 UI적용

---
+++ Assets/3.Script/No/Handler/NavigationHandler.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class NavigationHandler : MonoBehaviour
+{
+    [Header("Contents Room Panel Settings")]
+    public GameObject Lobby;
+    public GameObject Stage;
+    public GameObject CharacterRoom;
+    public GameObject Order;
+    public GameObject My;
+    
+    [Header("Render Texture Settings")]
+    public GameObject LobbyRender;
+    public GameObject StageRender;
+    public GameObject CharacterRender;
+    public GameObject OrderRender;
+    public GameObject MyRender;
+
+    [Header("Public Settings")]
+    public Button BackButton;
+
+    [Header("Lobby Button Settings")]
+    public Button StageRoomButton;
+    public Button CharacterRoomButton;
+    public Button OrderRoomButton;
+    public Button MyRoomButton;
+    
+    [Header("Chapter Settings")]
+    private GameObject SelectedChapter;
+    public Button Chapter1;
+    public Button Chapter2;
+    public Button Check;
+
+    [Header("Stage Settings")]
+    public GameObject Stage1;
+    public GameObject Stage2;
+    
+    void Start()
+    {
+        BackButton.onClick.AddListener(OnMoveLobby);
+        StageRoomButton.onClick.AddListener(OnMoveStageRoom);
+        CharacterRoomButton.onClick.AddListener(OnMoveCharacterRoom);
+        OrderRoomButton.onClick.AddListener(OnMoveOrderRoom);
+        MyRoomButton.onClick.AddListener(OnMoveMyRoom);
+        
+        Chapter1.onClick.AddListener(()=> OnSelectChapter(Stage1));
+        Chapter2.onClick.AddListener(()=> OnSelectChapter(Stage2));
+        
+        Check.onClick.AddListener(OnMoveChapter);
+    }
+
+    void ShowRoom(GameObject panel, GameObject render)
+    {
+        Lobby.SetActive(false);
+        Stage.SetActive(false);
+        CharacterRoom.SetActive(false);
+        Order.SetActive(false);
+        My.SetActive(false);
+        Stage1.SetActive(false);
+        Stage2.SetActive(false);
+        
+        LobbyRender.SetActive(false);
+        StageRender.SetActive(false);
+        CharacterRender.SetActive(false);
+        OrderRender.SetActive(false);
+        MyRender.SetActive(false);
+        
+        panel.SetActive(true);
+        render.SetActive(true);
+    }
+
+    private void OnMoveLobby() => ShowRoom(Lobby, LobbyRender);
+    private void OnMoveStageRoom() => ShowRoom(Stage, StageRender);
+    private void OnMoveCharacterRoom() => ShowRoom(CharacterRoom, CharacterRender);
+    private void OnMoveOrderRoom() => ShowRoom(Order, OrderRender);
+    private void OnMoveMyRoom() => ShowRoom(My, MyRender);
+    private void OnMoveChapter() => OnMoverStagePage();
+    
+    private void OnSelectChapter(GameObject stage)
+    {
+        SelectedChapter = stage;
+    }
+    
+    private void OnMoverStagePage()
+    {
+        Stage1.SetActive(false);
+        Stage2.SetActive(false);
+        SelectedChapter.SetActive(true);  
+    }
+    
+    // FireBase요청 필요하면 구조 변경할때 사용하면 됨
+    // void OnMoveLobby()
+    // {
+    //     
+    // }
+    //
+    // void OnMoveStageRoom()
+    // {
+    //     
+    // }
+    //
+    // void OnMoveCharacterRoom()
+    // {
+    //     
+    // }
+    //
+    // void OnMoveOrderRoom()
+    // {
+    //     
+    // }
+    //
+    // void OnMoveMyRoom()
+    // {
+    //     
+    // }
+}
