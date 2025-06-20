cf30eb81546d5236833dfc888bf6754b486254c0
bychan0v0 <bychan0v0@gmail.com>
2025-06-20 12:12:11 +0900

    무기패널 제작 중

--- Assets/3.Script/No/OutGameData/CharacterRoom.cs
+++ Assets/3.Script/No/OutGameData/CharacterRoom.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject characterRoomPanel;
 
-    // Update is called once per frame
-    void Update()
+    private CharacterDataSample currentCharacter;
+    
+    private void OnEnable()
     {
+        currentCharacter = FirebaseMainSession.Instance.FirebaseUser.playerData.HasCharacter[0];
         
     }
 }
