a1f7f33e8d75fdc640e091de2baaaf4edbfd4682
harangji <radio000507@gmail.com>
2025-06-21 17:07:13 +0900

    슈터 스킬 1 타임라인 수정 스킬 사용 전 비동기로 적을 향해 회전, 사용 후 원래 로테이션으로 복귀

--- Assets/3.Script/Ji/Camera/InGameCameraPD.cs
+++ Assets/3.Script/Ji/Camera/InGameCameraPD.cs
@@ -1,131 +1,131 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cinemachine;
-using Sirenix.OdinInspector;
-using UnityEngine;
-using UnityEngine.Playables;
-using UnityEngine.Timeline;
-using Random = UnityEngine.Random;
-using UnityEngine.Sequences.Timeline;
-
-public class InGameCameraPD : MonoBehaviour
-{
-    [SerializeField] private CinemachineVirtualCamera topViewCamera;
-    [SerializeField] private CinemachineVirtualCamera characterMiddleZoomCamera;
-    
-    [SerializeField] private PlayableDirector director;
-    [SerializeField] private TimelineAsset timelineAsset;
-    
-    [SerializeField] private Animator animator;
-    [SerializeField] private AnimationClip[] animationClip;
-    [SerializeField] private GameObject effect;
-    
-    [ReadOnly] private bool isPlaying = false;
-    
-    void Start()
-    {
-        director.Play(); // Timeline 재생
-    }
-    
-    void Update()
-    {
-        testCameraFocus();
-        // if (Input.GetKeyDown(KeyCode.Alpha1))
-        // {
-        //     PlaySkill(animator,animationClip[0], effect, timelineAsset);
-        // }
-        //
-        // if (Input.GetKeyDown(KeyCode.Alpha2))
-        // {
-        //     PlaySkill(animator,animationClip[1], effect, timelineAsset);
-        // }
-        //
-        // if (Input.GetKeyDown(KeyCode.Alpha3))
-        // {
-        //     PlaySkill(animator,animationClip[2], effect, timelineAsset);
-        // }
-    }
-
-    public void testCameraFocus()
-    {
-        // if (Input.GetMouseButtonDown(0)) // 마우스 클릭 (모바일은 터치로 변경 가능)
-        // {
-        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //     if (Physics.Raycast(ray, out RaycastHit hit, 100f))
-        //     {
-        //         // TestPlayerTimeLine timeline = hit.collider.GetComponent<TestPlayerTimeLine>();
-        //         // if (timeline != null)
-        //         // {
-        //         //     Debug.Log("TestPlayerTimeLine 대상 감지됨: " + hit.collider.name);
-        //         //
-        //         //     // 카메라를 해당 대상에 따라가게 설정
-        //         //     topViewCamera.LookAt = hit.transform;
-        //         //
-        //         //     // Priority 높여서 전환되게 설정
-        //         //     topViewCamera.Priority = 20;
-        //         // }
-        //     }
-        // }
-    }
-    
-    public void PlaySkillTimeline(Animator caster, TimelineAsset timelineAsset)
-    {
-        director.playableAsset = timelineAsset;
-
-        // 트랙 찾기 + 바인딩 (예: 애니메이션 트랙)
-        foreach (var track in timelineAsset.GetOutputTracks())
-        {
-            if (track is AnimationTrack)
-            {
-                director.SetGenericBinding(track, caster);
-            }
-            else if (track.name.Contains("Effect"))
-            {
-                // 이펙트용 트랙도 따로 바인딩 가능
-            }
-        }
-
-        director.Play();
-    }
-    
-    [CreateAssetMenu(menuName = "Skill/SkillDefinition")]
-    public class SkillDefinition : ScriptableObject
-    {
-        public string skillName;
-        public TimelineAsset timeline;
-        public GameObject projectilePrefab;
-        public float damage;
-    }
-    
-    public void PlaySkill(Animator caster, AnimationClip animClip, GameObject effect, TimelineAsset skillTimeLine)
-    {
-        // 1. 타임라인 설정
-        director.playableAsset = skillTimeLine;
-        
-        // 2. 애니메이션 트랙에 바인딩
-        foreach (var track in skillTimeLine.GetOutputTracks())
-        {
-            if (track is AnimationTrack animTrack)
-            {
-                director.SetGenericBinding(animTrack, caster);
-                // 3. 클립 교체 (중요!)
-                foreach (var clip in animTrack.GetClips())
-                {
-                    AnimationPlayableAsset animPlayableAsset = clip.asset as AnimationPlayableAsset;
-                    if (animPlayableAsset != null)
-                    {
-                        animPlayableAsset.clip = animClip; // 여기에 스킬별 애니메이션 클립 넣기!
-                        
-                        clip.start = Random.Range(0.0f, 0.5f);              // Timeline 상 시작 시간 (초)
-                        clip.duration = Random.Range(0.7f, 2.5f);          // 클립 재생 시간
-                        clip.clipIn = Random.Range(0.0f, 2.5f);          // 애니메이션의 시작 지점 (클립 내부 offset)
-                    }
-                }
-            }
-        }
-        
-        director.time = 0.0;
-        director.Play();
-    }
-}
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Cinemachine;
+// using Sirenix.OdinInspector;
+// using UnityEngine;
+// using UnityEngine.Playables;
+// using UnityEngine.Timeline;
+// using Random = UnityEngine.Random;
+// using UnityEngine.Sequences.Timeline;
+//
+// public class InGameCameraPD : MonoBehaviour
+// {
+//     [SerializeField] private CinemachineVirtualCamera topViewCamera;
+//     [SerializeField] private CinemachineVirtualCamera characterMiddleZoomCamera;
+//     
+//     [SerializeField] private PlayableDirector director;
+//     [SerializeField] private TimelineAsset timelineAsset;
+//     
+//     [SerializeField] private Animator animator;
+//     [SerializeField] private AnimationClip[] animationClip;
+//     [SerializeField] private GameObject effect;
+//     
+//     [ReadOnly] private bool isPlaying = false;
+//     
+//     void Start()
+//     {
+//         director.Play(); // Timeline 재생
+//     }
+//     
+//     void Update()
+//     {
+//         testCameraFocus();
+//         // if (Input.GetKeyDown(KeyCode.Alpha1))
+//         // {
+//         //     PlaySkill(animator,animationClip[0], effect, timelineAsset);
+//         // }
+//         //
+//         // if (Input.GetKeyDown(KeyCode.Alpha2))
+//         // {
+//         //     PlaySkill(animator,animationClip[1], effect, timelineAsset);
+//         // }
+//         //
+//         // if (Input.GetKeyDown(KeyCode.Alpha3))
+//         // {
+//         //     PlaySkill(animator,animationClip[2], effect, timelineAsset);
+//         // }
+//     }
+//
+//     public void testCameraFocus()
+//     {
+//         // if (Input.GetMouseButtonDown(0)) // 마우스 클릭 (모바일은 터치로 변경 가능)
+//         // {
+//         //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//         //     if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+//         //     {
+//         //         // TestPlayerTimeLine timeline = hit.collider.GetComponent<TestPlayerTimeLine>();
+//         //         // if (timeline != null)
+//         //         // {
+//         //         //     Debug.Log("TestPlayerTimeLine 대상 감지됨: " + hit.collider.name);
+//         //         //
+//         //         //     // 카메라를 해당 대상에 따라가게 설정
+//         //         //     topViewCamera.LookAt = hit.transform;
+//         //         //
+//         //         //     // Priority 높여서 전환되게 설정
+//         //         //     topViewCamera.Priority = 20;
+//         //         // }
+//         //     }
+//         // }
+//     }
+//     
+//     public void PlaySkillTimeline(Animator caster, TimelineAsset timelineAsset)
+//     {
+//         director.playableAsset = timelineAsset;
+//
+//         // 트랙 찾기 + 바인딩 (예: 애니메이션 트랙)
+//         foreach (var track in timelineAsset.GetOutputTracks())
+//         {
+//             if (track is AnimationTrack)
+//             {
+//                 director.SetGenericBinding(track, caster);
+//             }
+//             else if (track.name.Contains("Effect"))
+//             {
+//                 // 이펙트용 트랙도 따로 바인딩 가능
+//             }
+//         }
+//
+//         director.Play();
+//     }
+//     
+//     [CreateAssetMenu(menuName = "Skill/SkillDefinition")]
+//     public class SkillDefinition : ScriptableObject
+//     {
+//         public string skillName;
+//         public TimelineAsset timeline;
+//         public GameObject projectilePrefab;
+//         public float damage;
+//     }
+//     
+//     public void PlaySkill(Animator caster, AnimationClip animClip, GameObject effect, TimelineAsset skillTimeLine)
+//     {
+//         // 1. 타임라인 설정
+//         director.playableAsset = skillTimeLine;
+//         
+//         // 2. 애니메이션 트랙에 바인딩
+//         foreach (var track in skillTimeLine.GetOutputTracks())
+//         {
+//             if (track is AnimationTrack animTrack)
+//             {
+//                 director.SetGenericBinding(animTrack, caster);
+//                 // 3. 클립 교체 (중요!)
+//                 foreach (var clip in animTrack.GetClips())
+//                 {
+//                     AnimationPlayableAsset animPlayableAsset = clip.asset as AnimationPlayableAsset;
+//                     if (animPlayableAsset != null)
+//                     {
+//                         animPlayableAsset.clip = animClip; // 여기에 스킬별 애니메이션 클립 넣기!
+//                         
+//                         clip.start = Random.Range(0.0f, 0.5f);              // Timeline 상 시작 시간 (초)
+//                         clip.duration = Random.Range(0.7f, 2.5f);          // 클립 재생 시간
+//                         clip.clipIn = Random.Range(0.0f, 2.5f);          // 애니메이션의 시작 지점 (클립 내부 offset)
+//                     }
+//                 }
+//             }
+//         }
+//         
+//         director.time = 0.0;
+//         director.Play();
+//     }
+// }
