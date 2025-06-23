316df46fe69575f80b6d547190daf46cc4976e89
TaeGun08 <gustn4984@naver.com>
2025-06-23 14:53:55 +0900

    자잘한 수정

--- Assets/3.Script/Ji/TimeLine/CustomBezierCurveTween/CustomBezierCurveTweenMixerBehaviour.cs
+++ Assets/3.Script/Ji/TimeLine/CustomBezierCurveTween/CustomBezierCurveTweenMixerBehaviour.cs
@@ -189,7 +189,7 @@ public class CustomBezierCurveTweenMixerBehaviour : PlayableBehaviour
         m_FirstFrameHappened = false; // 다음에 다시 실행되도록
         isProjectileActive = false;
         
-        if(cashedHitParticle)
+        if(cashedFlashParticle)
             cashedFlashParticle.SetActive(false);
         if(cashedHitParticle)
             cashedHitParticle.SetActive(false);
